6a54b154811936405c7b84ae595fbef38b6e1eb0
Dan Beaven <dm.beaven@gmail.com>
2020-03-18 17:26:01 +0000

    restructure lambda files

--- lambdas/handler.ts
+++ lambdas/handler.ts
@@ -1,17 +1,17 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
 
-import dynamoClient from './src/lib/dynamodb'
+import { getGroups } from './src/data'
 
-export const groups: APIGatewayProxyHandler = async (_event, _context) => {
-  const groups = await dynamoClient
-    .scan({ TableName: process.env.DYNAMODB_TABLE as string })
-    .promise()
-    .catch(err => err.message)
-    .then(x => x.Items)
-
-  return {
+export const groups: APIGatewayProxyHandler = async (_event, _context) =>
+  getGroups().then(groups => ({
     statusCode: 200,
     body: JSON.stringify(groups),
+  }))
+
+export const group: APIGatewayProxyHandler = async (_event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(_event),
   }
 }
