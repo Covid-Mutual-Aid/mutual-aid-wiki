ad10973e33f0d985c29e66d75d1e988245bcd573
Julian <julian.tapales@gmail.com>
2020-03-18 12:09:39 +0000

    Create <GroupsTable/>

---
+++ client/src/components/GroupsTable.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+import { Group } from '../utils/types'
+
+type Props = {
+  groups: Group[]
+}
+const GroupsTable = ({ groups }: Props) => {
+  return (
+    <Table responsive striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        {groups.map((group, i) => (
+          <tr key={i}>
+            <td>{group.name}</td>
+            <td>{group.location_name}</td>
+            <td>
+              <a href={group.link_facebook}>link</a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default GroupsTable
