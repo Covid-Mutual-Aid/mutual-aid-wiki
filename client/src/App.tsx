9329f79cfa6afa1397861bec5ad87ec7ff08edad
Julian <julian.tapales@gmail.com>
2020-03-18 01:46:47 +0000

    Initialise db with scraped data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useRequest } from './contexts/RequestProvider';
+import React, { useState, useEffect } from 'react'
+import { useRequest } from './contexts/RequestProvider'
 
 function App() {
-  const [groups, setGroups] = useState<any[]>([]);
-  const request = useRequest();
+  const [groups, setGroups] = useState<any[]>([])
+  const request = useRequest()
 
   useEffect(() => {
     request('/dev/groups').then(setGroups)
@@ -11,20 +11,19 @@ function App() {
 
   return (
     <div className="App">
-      <header className='App-header'>
+      <header className="App-header">
         <p>
-          Edit <code>src/App.tsx
-            </code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        {groups.map(group => (
-          <div key={group.id}>
+        {groups.map((group, i) => (
+          <div key={i}>
             <p>{group.name}</p>
-            <a href={group.facebook_link}>{group.facebook_link}</a>
+            <a href={group.link}>{group.link}</a>
           </div>
         ))}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
