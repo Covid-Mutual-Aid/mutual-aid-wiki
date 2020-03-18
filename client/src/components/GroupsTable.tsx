cebd270ba74749bd630335a178f3391ffc500648
Julian <julian.tapales@gmail.com>
2020-03-18 22:14:18 +0000

    Wrap <GroupsTable/> height

--- client/src/components/GroupsTable.tsx
+++ client/src/components/GroupsTable.tsx
@@ -9,24 +9,26 @@ type Props = {
 const GroupsTable = ({ groups }: Props) => {
   return (
     <div>
-      <Table className="table-fixed" responsive striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {groups.map((group, i) => (
-            <tr key={i}>
-              <td>
-                <a href={group.link_facebook}>{group.name}</a>
-              </td>
-              <td>{group.location_name}</td>
+      <div className="group-table-wrapper">
+        <Table className="table-fixed" responsive striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Location</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {groups.map((group, i) => (
+              <tr key={i}>
+                <td>
+                  <a href={group.link_facebook}>{group.name}</a>
+                </td>
+                <td>{group.location_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
   )
 }
