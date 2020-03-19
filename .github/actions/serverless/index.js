e55faf5cda97f0f1ff8c2d0509f7cac346cbc3bb
Dan Beaven <dm.beaven@gmail.com>
2020-03-19 19:30:00 +0000

    Test working dir workflow action

--- .github/actions/serverless/index.js
+++ .github/actions/serverless/index.js
@@ -1,6 +1,11 @@
 const { spawnSync } = require('child_process')
+const path = require('path')
+
 console.log('RUNNING')
-spawnSync('ls', {
-  cwd: process.cwd(),
-  stdio: 'inherit',
-})
+exceSync(
+  `sls --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} deploy`,
+  {
+    cwd: path.join(process.cwd(), 'lambdas'),
+    stdio: 'inherit',
+  }
+)
