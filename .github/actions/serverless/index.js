63cdb0a8a78fac06f1cdd38f3db56d4ac19814bb
Dan Beaven <dm.beaven@gmail.com>
2020-03-19 19:37:08 +0000

    Test working dir workflow action

--- .github/actions/serverless/index.js
+++ .github/actions/serverless/index.js
@@ -1,11 +1,15 @@
 const { execSync } = require('child_process')
 const path = require('path')
 
-console.log('RUNNING')
 execSync(
-  `sudo sls --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} deploy`,
+  `serverless config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY}`,
   {
     cwd: path.join(process.cwd(), 'lambdas'),
     stdio: 'inherit',
   }
 )
+
+execSync(`sudo sls deploy`, {
+  cwd: path.join(process.cwd(), 'lambdas'),
+  stdio: 'inherit',
+})
