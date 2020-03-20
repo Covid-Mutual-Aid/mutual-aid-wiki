44ced850f0cd9cdcf3b455c10b64aa208a472b97
Dan Beaven <dm.beaven@gmail.com>
2020-03-20 21:30:32 +0000

    Client request retries on  failure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -17,8 +17,14 @@ const baseUrl =
     ? '/dev'
     : 'https://sn29v7uuxi.execute-api.eu-west-2.amazonaws.com/dev'
 
+const request = <T extends any>(input: RequestInfo, init?: RequestInit, accum = 0): Promise<T> =>
+  fetch(baseUrl + input, init).then(x => x.json()).catch(err => {
+    if (accum > 5) return Promise.reject(err);
+    return new Promise(res => setTimeout(res, 500 * accum * accum)).then(() => request(input, init, accum + 1))
+  })
+
 const Render = () => (
-  <RequestProvider request={useCallback((input: RequestInfo, init?: RequestInit) => fetch(baseUrl + input, init).then(x => x.json()), [])}>
+  <RequestProvider request={request}>
     <MapProvider>
       <App />
     </MapProvider>
