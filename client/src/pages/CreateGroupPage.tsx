baa6aa8ad3157f7c5b42e2022e5dc6ef9d7480be
Julian <julian.tapales@gmail.com>
2020-03-21 02:10:38 +0000

    Sanitised url

--- client/src/pages/CreateGroupPage.tsx
+++ client/src/pages/CreateGroupPage.tsx
@@ -5,13 +5,28 @@ import React, { useState } from 'react'
 import { useRequest } from '../contexts/RequestProvider'
 import Location from '../components/Location'
 
+// https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+function validURL(str: string) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // fragment locator
+  return !!pattern.test(str)
+}
+
 const CreateGroup = () => {
   const request = useRequest()
-  const history = useHistory();
+  const history = useHistory()
   const [valid, setValid] = useState(true)
   const [name, setName] = useState('')
   const [link, setLink] = useState('')
   const [location, setLocation] = useState<{ lat: string; lng: string; name: string } | null>(null)
+  const [error, setError] = useState('')
 
   return (
     <div style={{ width: '100%', padding: '1.5rem' }}>
@@ -19,6 +34,10 @@ const CreateGroup = () => {
         style={{ maxWidth: '50rem', margin: '0 auto' }}
         onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
           e.preventDefault()
+          if (!validURL(link)) {
+            setError('Please enter a valid URL')
+            return
+          }
           if (!location || !link || !name) return setValid(false)
           request(`/group/create`, {
             method: 'POST',
@@ -35,7 +54,11 @@ const CreateGroup = () => {
           <Form.Control placeholder="Group name" onChange={(e: any) => setName(e.target.value)} />
         </Form.Group>
         <Form.Group>
-          <Form.Control placeholder="Facebook link" onChange={(e: any) => setLink(e.target.value)} />
+          <Form.Text className="text-muted">{error}</Form.Text>
+          <Form.Control
+            placeholder="Facebook link"
+            onChange={(e: any) => setLink(e.target.value)}
+          />
         </Form.Group>
         <Form.Group>
           <Location onChange={setLocation} placeholder={'Group location'} />
@@ -45,10 +68,9 @@ const CreateGroup = () => {
           <Link to="/" style={{ marginRight: '1rem' }}>
             <Button variant="light">Cancel</Button>
           </Link>
-          <Button variant="primary" type="submit" >
+          <Button variant="primary" type="submit">
             Add Group
           </Button>
-
         </div>
       </Form>
     </div>
