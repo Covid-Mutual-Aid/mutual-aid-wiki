788ea4271e7d408bbd64f4f3222dd97cbeaefe50
Julian <julian.tapales@gmail.com>
2020-03-21 08:40:19 +0000

    Remove react-flip-move
    Not optimised to work on large elements. It should only animate items within the viewport :-(

--- client/src/components/GroupsTable.tsx
+++ client/src/components/GroupsTable.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Table, Badge } from 'react-bootstrap'
-import FlipMove from 'react-flip-move'
 
 import { Group } from '../utils/types'
 import { useMap } from '../contexts/MapProvider'
@@ -26,36 +25,32 @@ const GroupsTable = ({ groups, shouldDisplayDistance }: Props) => {
             </tr>
           </thead>
           <tbody>
-            <FlipMove>
-              {groups.map(
-                ({ link_facebook, name, location_name, distance, location_coord, id }) => (
-                  <tr key={id}>
-                    <td
-                      style={{ cursor: 'pointer' }}
-                      onClick={() =>
-                        setMapState({
-                          zoom: 11,
-                          group: { id, link_facebook, name, location_name, location_coord },
-                          center: location_coord,
-                        })
-                      }
-                    >
-                      {location_name}{' '}
-                      {distance && shouldDisplayDistance ? (
-                        <Badge variant="success">{(distance / 1000).toFixed(1) + 'km'}</Badge>
-                      ) : (
-                        ''
-                      )}
-                    </td>
-                    <td>
-                      <a target="_blank" rel="noopener noreferrer" href={link_facebook}>
-                        {name}
-                      </a>
-                    </td>
-                  </tr>
-                )
-              )}
-            </FlipMove>
+            {groups.map(({ link_facebook, name, location_name, distance, location_coord, id }) => (
+              <tr key={id}>
+                <td
+                  style={{ cursor: 'pointer' }}
+                  onClick={() =>
+                    setMapState({
+                      zoom: 11,
+                      group: { id, link_facebook, name, location_name, location_coord },
+                      center: location_coord,
+                    })
+                  }
+                >
+                  {location_name}{' '}
+                  {distance && shouldDisplayDistance ? (
+                    <Badge variant="success">{(distance / 1000).toFixed(1) + 'km'}</Badge>
+                  ) : (
+                    ''
+                  )}
+                </td>
+                <td>
+                  <a target="_blank" rel="noopener noreferrer" href={link_facebook}>
+                    {name}
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </div>
