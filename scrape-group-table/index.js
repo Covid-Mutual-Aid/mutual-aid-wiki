640ac34e48d276827fa76605e2a749232f9d7f8b
Julian <julian.tapales@gmail.com>
2020-03-21 14:37:14 +0000

    Remove depreceated scraping code

--- scrape-group-table/index.js
+++
@@ -1,2 +0,0 @@
-console.log('Currently using each script separately, `node scrape.js` then `node geolocate.js`')
-console.log('Check comment in commit: 2f1ec9a81417be456cfdabf9bf77ed82073c434d for more info')
