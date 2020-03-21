f48a6e8e3b2f93701ca1486403a904f1fc8b8775
Julian <julian.tapales@gmail.com>
2020-03-21 00:55:14 +0000

    Reorder group table columns

--- client/src/components/GroupsTable.tsx
+++ client/src/components/GroupsTable.tsx
@@ -20,34 +20,34 @@ const GroupsTable = ({ groups, shouldDisplayDistance }: Props) => {
         <Table className="table-fixed" responsive striped bordered hover size="sm">
           <thead>
             <tr>
-              <th>Name</th>
               <th>Location</th>
+              <th>Name</th>
             </tr>
           </thead>
           <tbody>
             {groups.map(({ link_facebook, name, location_name, distance, location_coord, id }) => (
-              <tr
-                key={id}
-                onClick={() =>
-                  setMapState({
-                    zoom: 11,
-                    group: { id, link_facebook, name, location_name, location_coord },
-                    center: location_coord,
-                  })
-                }
-              >
-                <td>
-                  <a target="_blank" rel="noopener noreferrer" href={link_facebook}>
-                    {name}
-                  </a>
-                </td>
-                <td>
+              <tr key={id}>
+                <td
+                  style={{ cursor: 'pointer' }}
+                  onClick={() =>
+                    setMapState({
+                      zoom: 11,
+                      group: { id, link_facebook, name, location_name, location_coord },
+                      center: location_coord,
+                    })
+                  }
+                >
                   {location_name}{' '}
                   {distance && shouldDisplayDistance ? (
                     <Badge variant="success">{(distance / 1000).toFixed(1) + 'km'}</Badge>
                   ) : (
                     ''
                   )}
+                </td>
+                <td>
+                  <a target="_blank" rel="noopener noreferrer" href={link_facebook}>
+                    {name}
+                  </a>
                 </td>
               </tr>
             ))}
