a8547d064028d58f74c05a8a7bcbd4bb19df9bb7
Julian <julian.tapales@gmail.com>
2020-03-22 14:34:18 +0000

    Rollback to use google sheets “addd group” form

--- client/src/pages/GroupsMapPage.tsx
+++ client/src/pages/GroupsMapPage.tsx
@@ -72,13 +72,13 @@ function GroupsMapPage() {
                   </Button>
                 </Col>
                 <Col xs={5} md={2} className="input-height">
-                  <Link to="/create-group">
+                  {/* <Link to="/create-group">
                     <Button className="full-width" variant="secondary">
                       Add Group
                     </Button>
-                  </Link>
+                  </Link> */}
                   {/* Maintain Google Doc as primary source of info */}
-                  {/* <a
+                  <a
                     target="_blank"
                     href="https://docs.google.com/forms/d/e/1FAIpQLSdJrgqHazomhDsJDG3Nnye30Ys7sZEl-APCrQh80D1g-iQrgQ/viewform"
                   >
@@ -89,7 +89,7 @@ function GroupsMapPage() {
                     >
                       Add Group
                     </Button>
-                  </a> */}
+                  </a>
                 </Col>
               </Form.Row>
             </Form>
