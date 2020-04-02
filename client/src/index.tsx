8e57d26069c24cfb0de1b4c73250fbbf92a488d3
Dan Beaven <dm.beaven@gmail.com>
2020-04-02 23:53:39 +0100

    Stopped request refetching on  failure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -22,14 +22,7 @@ const baseUrl =
     : 'https://sn29v7uuxi.execute-api.eu-west-2.amazonaws.com/dev'
 
 const request = <T extends any>(input: RequestInfo, init?: RequestInit, accum = 0): Promise<T> =>
-  fetch(baseUrl + input, init)
-    .then(x => x.json())
-    .catch(err => {
-      if (accum > 2) return Promise.reject(err)
-      return new Promise(res => setTimeout(res, 500 * accum * accum)).then(() =>
-        request(input, init, accum + 1)
-      )
-    })
+  fetch(baseUrl + input, init).then(x => x.json())
 
 const Render = () => (
   <RequestProvider request={request}>
