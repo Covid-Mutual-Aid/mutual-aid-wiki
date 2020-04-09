5a01ca755080d6eb0286c59350a49cde0e9c23aa
Dan Beaven <dm.beaven@gmail.com>
2020-04-09 15:41:13 +0100

    Add ids to response groups

--- lambdas/services/database/handler.ts
+++ lambdas/services/database/handler.ts
@@ -14,8 +14,8 @@ const { create, remove, readAll } = createDynamoApi<Group>(TableName)
 export const createNoDuplicates = (
   group: Omit<Group, 'id' | 'pub_id'> & { id?: string; pub_id?: string }
 ) =>
-  readAll().then(groups =>
-    groups.some(g => isSameGroup(g, group))
+  readAll().then((groups) =>
+    groups.some((g) => isSameGroup(g, group))
       ? 'Exists'
       : create({ ...group, created_at: Date.now() }).then(() => 'Added')
   )
@@ -25,23 +25,24 @@ const sanitize = (x?: Group) =>
   !x
     ? null
     : {
+        id: x.pub_id,
         name: x.name,
         link_facebook: x.link_facebook,
         location_name: x.location_name,
         location_coord: x.location_coord,
       }
 
-export const getGroup = lambda.queryParams()(x =>
-  readAll().then(groups =>
+export const getGroup = lambda.queryParams()((x) =>
+  readAll().then((groups) =>
     x && x.id
-      ? sanitize(groups.find(y => y.id === x.id || y.pub_id === x.id))
+      ? sanitize(groups.find((y) => y.id === x.id || y.pub_id === x.id))
       : groups.map(sanitize)
   )
 )
 
-export const updateGroup = lambda.body()(group =>
-  readAll().then(groups => {
-    const grp = groups.find(x => x.id === group.id)
+export const updateGroup = lambda.body()((group) =>
+  readAll().then((groups) => {
+    const grp = groups.find((x) => x.id === group.id)
     if (!grp) return Promise.reject('No group with id')
     return create({ updated_at: Date.now(), ...grp, ...omit('id')(group) })
   })
@@ -58,7 +59,7 @@ export const createGroup = lambda.body(
       lng: P.number,
     }),
   })
-)(group =>
+)((group) =>
   isOffline()
     ? (createNoDuplicates(group) as any)
     : (Promise.all([createNoDuplicates(group), addSheetRow(group)]) as any)
