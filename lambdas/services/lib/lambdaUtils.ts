95c32337abc861b07bed126587b2011036d2db1c
Dan Beaven <dm.beaven@gmail.com>
2020-04-09 16:36:01 +0100

    update ts-prove

--- lambdas/services/lib/lambdaUtils.ts
+++ lambdas/services/lib/lambdaUtils.ts
@@ -1,9 +1,9 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda'
-import { Proof, isSuccess, ProofType } from 'ts-prove'
+import { Proof, isProved, ProofType } from 'ts-prove'
 
 const promiseProof = <T extends any>(proof: Proof<T>) => (arg: any) => {
   const result = proof(arg)
-  if (isSuccess(result)) return Promise.resolve(result[1])
+  if (Array.isArray(result) && isProved(result)) return Promise.resolve(result[1])
   return Promise.reject((result as any)[0])
 }
 
@@ -12,18 +12,18 @@ const lambda = (callback: (event: APIGatewayProxyEvent, context: Context) => any
   context: Context
 ): Promise<APIGatewayProxyResult> =>
   Promise.resolve(callback(event, context))
-    .then(res => ({
+    .then((res) => ({
       statusCode: 200,
       body: JSON.stringify(res),
       headers: { 'Access-Control-Allow-Origin': '*' },
     }))
-    .catch(err => ({
+    .catch((err) => ({
       statusCode: 500,
       body: err.message || err,
     }))
 
 lambda.body = <T extends Proof<any>>(proof?: T) => (callback: (x: ProofType<T>) => Promise<any>) =>
-  lambda(event =>
+  lambda((event) =>
     Promise.resolve(
       !proof ? JSON.parse(event.body || '{}') : promiseProof(proof)(JSON.parse(event.body || '{}'))
     ).then(callback)
@@ -32,7 +32,7 @@ lambda.body = <T extends Proof<any>>(proof?: T) => (callback: (x: ProofType<T>) 
 lambda.queryParams = <T extends Proof<any>>(proof?: T) => (
   callback: (x: ProofType<T>) => Promise<any>
 ) =>
-  lambda(event =>
+  lambda((event) =>
     Promise.resolve(
       !proof
         ? event.queryStringParameters || ({} as any)
