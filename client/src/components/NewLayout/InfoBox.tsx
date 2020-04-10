e67b97e3545f984f6dd790c37d1b2323efd81215
Dan Beaven <dm.beaven@gmail.com>
2020-04-10 13:40:45 +0100

    add selected to top

--- client/src/components/NewLayout/InfoBox.tsx
+++ client/src/components/NewLayout/InfoBox.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { usePlaceState, usePlaceMethod } from '../../contexts/StateContext'
+import { useData } from '../../contexts/DataProvider'
 
 const InfoBox = () => {
+  const { groups } = useData()
   const { onSearch } = usePlaceMethod()
   const {
     search: { place },
+    selected,
   } = usePlaceState()
 
+  const selectedGroup = groups.find((x) => x.id === selected)
   return (
     <div>
       {place && (
@@ -16,6 +20,13 @@ const InfoBox = () => {
             <button type="button" className="clear" onClick={() => onSearch()}>
               clear
             </button>
+          </div>
+        </>
+      )}
+      {selectedGroup && (
+        <>
+          <div style={{ padding: '1rem 0' }}>
+            <h4>{selectedGroup.name}</h4>
           </div>
         </>
       )}
