02a67a4a7b1b0ec2bd655199ab415c7f7722b46d
Julian <julian.tapales@gmail.com>
2020-04-11 23:29:55 +0100

    Begin styling secondary pages

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -5,58 +5,55 @@ import { TextWrapper } from '../components/NewLayout/StyledComponents'
 const AboutPage = () => {
   return (
     <SecondaryPage title="About">
-      <TextWrapper>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Mattis ullamcorper velit sed ullamcorper morbi tincidunt
-          ornare. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Diam quis enim lobortis
-          scelerisque fermentum dui faucibus. Quis hendrerit dolor magna eget. Nibh tortor id
-          aliquet lectus proin nibh nisl condimentum. Eu nisl nunc mi ipsum faucibus vitae aliquet
-          nec. Odio pellentesque diam volutpat commodo sed egestas. Ipsum nunc aliquet bibendum enim
-          facilisis gravida neque convallis. Volutpat lacus laoreet non curabitur gravida arcu ac.
-          Sodales ut etiam sit amet nisl. Sit amet consectetur adipiscing elit. Morbi tristique
-          senectus et netus et malesuada. Adipiscing commodo elit at imperdiet dui accumsan sit
-          amet. Eget gravida cum sociis natoque penatibus et magnis. Ut tellus elementum sagittis
-          vitae et leo. Amet aliquam id diam maecenas ultricies. Varius vel pharetra vel turpis nunc
-          eget lorem dolor sed. Nunc congue nisi vitae suscipit tellus mauris a. Odio ut enim
-          blandit volutpat. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis
-          natoque. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Nam libero
-          justo laoreet sit amet cursus sit. Sollicitudin nibh sit amet commodo nulla facilisi
-          nullam vehicula. Mauris in aliquam sem fringilla ut morbi tincidunt. Faucibus interdum
-          posuere lorem ipsum. Purus in massa tempor nec feugiat. Mi sit amet mauris commodo quis
-          imperdiet massa tincidunt nunc. Volutpat odio facilisis mauris sit amet massa. In
-          hendrerit gravida rutrum quisque non tellus. Mattis rhoncus urna neque viverra justo nec
-          ultrices dui. Consectetur adipiscing elit duis tristique sollicitudin nibh sit. Nisi porta
-          lorem mollis aliquam. Ipsum dolor sit amet consectetur adipiscing elit ut. Scelerisque
-          mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Posuere
-          urna nec tincidunt praesent semper. Bibendum ut tristique et egestas quis ipsum. Sit amet
-          porttitor eget dolor morbi. Vitae nunc sed velit dignissim. Cursus in hac habitasse platea
-          dictumst quisque. Porta lorem mollis aliquam ut porttitor leo a. Vitae nunc sed velit
-          dignissim sodales ut eu sem integer. Enim blandit volutpat maecenas volutpat blandit
-          aliquam etiam. Sodales ut eu sem integer vitae justo eget magna fermentum. Nulla
-          pellentesque dignissim enim sit amet venenatis urna. Consequat interdum varius sit amet
-          mattis vulputate enim nulla aliquet. Augue interdum velit euismod in pellentesque massa
-          placerat duis. Sapien faucibus et molestie ac. Sed ullamcorper morbi tincidunt ornare
-          massa eget egestas purus. Luctus accumsan tortor posuere ac. Sit amet nulla facilisi morbi
-          tempus. Ultrices gravida dictum fusce ut placerat orci nulla. Fermentum leo vel orci porta
-          non pulvinar neque. Pellentesque pulvinar pellentesque habitant morbi tristique senectus
-          et. Et tortor at risus viverra adipiscing at in. Sit amet consectetur adipiscing elit.
-          Odio pellentesque diam volutpat commodo sed egestas. Fames ac turpis egestas maecenas
-          pharetra convallis posuere morbi leo. Fermentum odio eu feugiat pretium nibh ipsum
-          consequat. Adipiscing tristique risus nec feugiat. Sed elementum tempus egestas sed.
-          Pretium viverra suspendisse potenti nullam ac tortor vitae purus. Mauris commodo quis
-          imperdiet massa tincidunt. Proin fermentum leo vel orci. Vulputate dignissim suspendisse
-          in est. Duis convallis convallis tellus id interdum velit laoreet id donec. Velit egestas
-          dui id ornare arcu odio. Nulla pharetra diam sit amet nisl. Risus at ultrices mi tempus
-          imperdiet. Fermentum odio eu feugiat pretium nibh ipsum. Justo nec ultrices dui sapien
-          eget mi proin sed. Vitae et leo duis ut diam quam. Mauris pharetra et ultrices neque
-          ornare aenean euismod elementum nisi. Vel turpis nunc eget lorem dolor sed viverra ipsum.
-          Eu mi bibendum neque egestas congue. Sit amet consectetur adipiscing elit ut aliquam
-          purus. Quisque sagittis purus sit amet volutpat consequat mauris. Metus vulputate eu
-          scelerisque felis. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus.
-          Eget gravida cum sociis natoque penatibus.
-        </p>
-      </TextWrapper>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Mattis ullamcorper velit sed ullamcorper morbi tincidunt
+        ornare. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Diam quis enim lobortis
+        scelerisque fermentum dui faucibus. Quis hendrerit dolor magna eget. Nibh tortor id aliquet
+        lectus proin nibh nisl condimentum. Eu nisl nunc mi ipsum faucibus vitae aliquet nec. Odio
+        pellentesque diam volutpat commodo sed egestas. Ipsum nunc aliquet bibendum enim facilisis
+        gravida neque convallis. Volutpat lacus laoreet non curabitur gravida arcu ac. Sodales ut
+        etiam sit amet nisl. Sit amet consectetur adipiscing elit. Morbi tristique senectus et netus
+        et malesuada. Adipiscing commodo elit at imperdiet dui accumsan sit amet. Eget gravida cum
+        sociis natoque penatibus et magnis. Ut tellus elementum sagittis vitae et leo. Amet aliquam
+        id diam maecenas ultricies. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Nunc
+        congue nisi vitae suscipit tellus mauris a. Odio ut enim blandit volutpat. Imperdiet nulla
+        malesuada pellentesque elit eget gravida cum sociis natoque. Elementum nisi quis eleifend
+        quam adipiscing vitae proin sagittis. Nam libero justo laoreet sit amet cursus sit.
+        Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Mauris in aliquam sem
+        fringilla ut morbi tincidunt. Faucibus interdum posuere lorem ipsum. Purus in massa tempor
+        nec feugiat. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Volutpat odio
+        facilisis mauris sit amet massa. In hendrerit gravida rutrum quisque non tellus. Mattis
+        rhoncus urna neque viverra justo nec ultrices dui. Consectetur adipiscing elit duis
+        tristique sollicitudin nibh sit. Nisi porta lorem mollis aliquam. Ipsum dolor sit amet
+        consectetur adipiscing elit ut. Scelerisque mauris pellentesque pulvinar pellentesque
+        habitant morbi tristique senectus et. Posuere urna nec tincidunt praesent semper. Bibendum
+        ut tristique et egestas quis ipsum. Sit amet porttitor eget dolor morbi. Vitae nunc sed
+        velit dignissim. Cursus in hac habitasse platea dictumst quisque. Porta lorem mollis aliquam
+        ut porttitor leo a. Vitae nunc sed velit dignissim sodales ut eu sem integer. Enim blandit
+        volutpat maecenas volutpat blandit aliquam etiam. Sodales ut eu sem integer vitae justo eget
+        magna fermentum. Nulla pellentesque dignissim enim sit amet venenatis urna. Consequat
+        interdum varius sit amet mattis vulputate enim nulla aliquet. Augue interdum velit euismod
+        in pellentesque massa placerat duis. Sapien faucibus et molestie ac. Sed ullamcorper morbi
+        tincidunt ornare massa eget egestas purus. Luctus accumsan tortor posuere ac. Sit amet nulla
+        facilisi morbi tempus. Ultrices gravida dictum fusce ut placerat orci nulla. Fermentum leo
+        vel orci porta non pulvinar neque. Pellentesque pulvinar pellentesque habitant morbi
+        tristique senectus et. Et tortor at risus viverra adipiscing at in. Sit amet consectetur
+        adipiscing elit. Odio pellentesque diam volutpat commodo sed egestas. Fames ac turpis
+        egestas maecenas pharetra convallis posuere morbi leo. Fermentum odio eu feugiat pretium
+        nibh ipsum consequat. Adipiscing tristique risus nec feugiat. Sed elementum tempus egestas
+        sed. Pretium viverra suspendisse potenti nullam ac tortor vitae purus. Mauris commodo quis
+        imperdiet massa tincidunt. Proin fermentum leo vel orci. Vulputate dignissim suspendisse in
+        est. Duis convallis convallis tellus id interdum velit laoreet id donec. Velit egestas dui
+        id ornare arcu odio. Nulla pharetra diam sit amet nisl. Risus at ultrices mi tempus
+        imperdiet. Fermentum odio eu feugiat pretium nibh ipsum. Justo nec ultrices dui sapien eget
+        mi proin sed. Vitae et leo duis ut diam quam. Mauris pharetra et ultrices neque ornare
+        aenean euismod elementum nisi. Vel turpis nunc eget lorem dolor sed viverra ipsum. Eu mi
+        bibendum neque egestas congue. Sit amet consectetur adipiscing elit ut aliquam purus.
+        Quisque sagittis purus sit amet volutpat consequat mauris. Metus vulputate eu scelerisque
+        felis. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Eget gravida cum
+        sociis natoque penatibus.
+      </p>
     </SecondaryPage>
   )
 }
--- client/src/pages/SecondaryPage.tsx
+++ client/src/pages/SecondaryPage.tsx
@@ -7,28 +7,114 @@ import '../styles/new-layout.css'
 import icons from '../utils/icons'
 
 const SecondaryPage = ({ title, children }: { title: string; children: ReactChild }) => {
+  const list = [
+    'Who made this',
+    'Is my data secure',
+    'Is this Open Source',
+    'How can I edit my data',
+    'Can I remove my data',
+    'How can I prevent people spamming my group',
+    'Report a group',
+    'Add language translations',
+  ]
   return (
-    <Wrapper>
-      <div>
-        <Nav>
-          <Link to="/newlayout">{icons('chevronL')}</Link>
-        </Nav>
-        <h3>{title}</h3>
+    <Page>
+      <div className="wrapper">
+        <div className="nav-contents">
+          <Nav>
+            <Link to="/newlayout">{icons('chevronL')}</Link>
+          </Nav>
+          <h3>{title}</h3>
+          <div className="section-list">
+            {list.map((t, i) => (
+              <p>{t}</p>
+            ))}
+          </div>
+        </div>
+        <div className="content">{children}</div>
       </div>
-      {children}
-    </Wrapper>
+    </Page>
   )
 }
 
-const Wrapper = styled.div`
-  padding: 2.6rem;
+const Page = styled.div`
+  padding: 2.6rem 2rem;
   height: 100vh;
   overflow-x: scroll;
+  display: flex;
+  justify-content: center;
+
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    max-width: 64rem;
+  }
+
+  .nav-contents {
+    min-width: 24rem;
+    padding: 0 4rem;
+    position: sticky;
+    top: 0;
+    align-self: flex-start;
+  }
+
+  .section-list {
+    /* font-family: 'Source Serif Pro', serif; */
+    margin-top: 2.6rem;
+    padding-left: 1.2rem;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .content {
+    font-size: 1.12rem;
+    font-weight: 500;
+
+    padding: 0 2rem;
+    padding-top: 12rem;
+
+    p {
+      font-family: 'Source Serif Pro', serif;
+      padding: 2rem 0;
+    }
+  }
 
   h3 {
     padding: 2rem 1rem;
     font-size: 3rem;
     font-weight: 800;
+  }
+
+  @media (max-width: 900px) {
+    .wrapper {
+      max-width: 40rem;
+      justify-content: flex-start;
+      flex-direction: column;
+    }
+
+    .content {
+      padding-top: 2rem;
+    }
+
+    .nav-contents {
+      min-width: 100%;
+      position: relative;
+      padding: 0 2rem;
+    }
+
+    .content-wrapper {
+      padding-top: 2rem;
+    }
+
+    .nav-contents > * {
+      flex: 1 1 160px;
+    }
+
+    .section-list {
+      flex-wrap: wrap;
+      column-count: 2;
+      margin-top: 0rem;
+    }
   }
 `
 
