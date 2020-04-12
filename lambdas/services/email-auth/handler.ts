b17caa7c35235a18d7640b6845649729796d8d76
Dan Beaven <dm.beaven@gmail.com>
2020-04-12 14:41:47 +0100

    fix no origin

--- lambdas/services/email-auth/handler.ts
+++ lambdas/services/email-auth/handler.ts
@@ -11,6 +11,9 @@ export const emailAuthLink = lambda(
       .then((grps) => grps.filter((x) => x.emails?.includes(body.email)))
       .then((grps) => (grps.length === 0 ? Promise.reject('No group with email exists') : grps))
       .then((grps) => signForGroups(grps.map((x) => x.id) as string[]))
-      .then((token) => ({ edit_url: this.event.headers.Origin + `/group/edit?token=${token}` }))
+      .then((token) => ({
+        ...this.event,
+        edit_url: (this.event.headers.Origin || '{NO ORIGIN}') + `/group/edit?token=${token}`,
+      }))
   })
 )
