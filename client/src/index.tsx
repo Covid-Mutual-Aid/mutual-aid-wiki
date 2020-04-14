57f405f0a259cd1a197c1270dbbe7356f59c49fc
Dan Beaven <dm.beaven@gmail.com>
2020-04-14 19:57:18 +0100

    fix client redirects

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -19,7 +19,7 @@ import StateProvider from './contexts/StateContext'
 Sentry.init({ dsn: 'https://54b6389bc04849729985b907d7dfcffe@sentry.io/5169267' })
 
 const request = <T extends any>(input: RequestInfo, init?: RequestInit, accum = 0): Promise<T> =>
-  fetch('/api/' + input, init).then((x) => x.json())
+  fetch('/api' + input, init).then((x) => x.json())
 
 fetch('/api/group/get')
   .then((res) => res.json())
