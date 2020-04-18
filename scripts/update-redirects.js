cafce8615ab4ef93ba24b49902d9f66c515337a5
Dan Beaven <dm.beaven@gmail.com>
2020-04-18 17:12:07 +0100

    fix deploy CI

--- scripts/update-redirects.js
+++ scripts/update-redirects.js
@@ -5,6 +5,20 @@ const endpoint = require('../stack.json').ServiceEndpoint
 
 const redirects = fs.readFileSync(path.join(__dirname, '../client/_redirects'), 'utf8')
 const modified = redirects.replace(/http.*?:splat/gim, `${endpoint}/:splat`)
-fs.writeFileSync(path.join(__dirname, '../client/_redirects'), modified)
 
-child.execSync(`##[set-output name=modified;]"${modified === redirects ? 'FALSE' : 'TRUE'}"`)
+const ghToken = process.env.GITHUB_TOKEN
+const branch = process.env.BRANCH
+
+if (redirects !== modified) {
+  fs.writeFileSync(path.join(__dirname, '../client/_redirects'), modified)
+
+  child.spawnSync('git', ['add', './client/_redirects'], { stdio: 'inherit' })
+  child.spawnSync('git', ['config', 'user.name', 'Action bot'], { stdio: 'inherit' })
+  child.spawnSync('git', ['config', 'user.email', 'bot@action.com'], { stdio: 'inherit' })
+  child.spawnSync('git', ['commit', '-m', '"Update redirects"'], { stdio: 'inherit' })
+  child.spawnSync(
+    'git',
+    ['push', '--quiet', `"https://${ghToken}@github.com/Covid-Mutual-Aid/groups-map.git"`, branch],
+    { stdio: 'inherit' }
+  )
+}
