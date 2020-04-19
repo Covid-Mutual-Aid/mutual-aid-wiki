949b69e1685fca39413941b34de85b828b4bc954
Dan Beaven <dm.beaven@gmail.com>
2020-04-19 23:53:55 +0100

    fixed origin

--- lambdas/services/_utility_/lib/lambdaRx.ts
+++ lambdas/services/_utility_/lib/lambdaRx.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda
 import { Proof, ProofType, isProved } from 'ts-prove'
 
 import { of, Observable, throwError, OperatorFunction, ObservableInput, forkJoin } from 'rxjs'
-import { map, mergeMap } from 'rxjs/operators'
+import { map, mergeMap, switchMap } from 'rxjs/operators'
 
 import { requestFailed } from '../logging'
 
@@ -15,7 +15,15 @@ type OpR<O> = O extends OperatorFunction<any, infer D> ? D : never
 const lambdaRx = (
   req$: (payload: Observable<LambdaInput>) => Observable<APIGatewayProxyResult>
 ) => (_event: APIGatewayProxyEvent, _context: Context) =>
-  req$(of({ _event, _context }))
+  req$(
+    of({ _event, _context }).pipe(
+      switchMap((input) => {
+        if (/localhost|mutualaid\.wiki/.test(input._event.headers['x-forwarded-host']))
+          return of(input)
+        return Promise.reject('Invalid origin')
+      })
+    )
+  )
     .toPromise()
     .catch((err) => {
       console.log(err)
@@ -63,7 +71,7 @@ export const prove = <P extends Proof<any>>(proof: P) => <T extends Record<strin
 
 export const responseJson$ = map((res) => ({
   statusCode: 200,
-  headers: { 'Access-Control-Allow-Origin': 'mutualaid.wiki' },
+  headers: { 'Access-Control-Allow-Origin': '*' },
   body: JSON.stringify(res),
 }))
 
