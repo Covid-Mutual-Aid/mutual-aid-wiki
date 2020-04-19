031d870c6c002cfb65a17b7853b2db0000c40c27
Julian <julian.tapales@gmail.com>
2020-04-19 23:59:37 +0100

    Correct success modal text on <Report/>

--- client/src/pages/Report.tsx
+++ client/src/pages/Report.tsx
@@ -18,17 +18,19 @@ const Report = () => {
     <CenterAlign>
       {successModal ? (
         <div style={{ textAlign: 'center', padding: '4rem', color: '#28a745' }}>
-          <h3>Thanks for submitting your group</h3>
+          <h3>Thanks for your report</h3>
         </div>
       ) : (
         <form
           onSubmit={(e) => {
+            e.preventDefault()
             if (!validated) return
             request('/request/report', {
               method: 'POST',
-              body: JSON.stringify({ message }),
+              body: JSON.stringify({ id, message }),
             })
               .then((x) => {
+                console.log('sucess')
                 setSuccessModal(true)
                 return new Promise((res) => setTimeout(res, 6000))
               })
