61c1b6c602438433c948b0e1bf5c1dcc1e6f103c
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2020-04-23 12:45:56 +0200

    WIP commit while rebasing other branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { Switch, Route } from 'react-router-dom'
 import React from 'react'
 
+
+import LocaleSwitcher from './components/LocaleSwitcher'
+
 import Groups from './pages/Groups'
 import About from './pages/About'
 
@@ -11,14 +14,17 @@ import Report from './pages/Report'
 
 function App() {
   return (
-    <Switch>
-      <Route path="/about" component={About} />
-      <Route path="/add-group" component={CreateGroup} />
-      <Route path="/edit/:id/:token" component={EditGroup} />
-      <Route path="/edit/:id" component={EmailAuth} />
-      <Route path="/report/:id" component={Report} />
-      <Route path="/" component={Groups} />
-    </Switch>
+    <div>
+      <LocaleSwitcher />
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/add-group" component={CreateGroup} />
+        <Route path="/edit/:id/:token" component={EditGroup} />
+        <Route path="/edit/:id" component={EmailAuth} />
+        <Route path="/report/:id" component={Report} />
+        <Route path="/" component={Groups} />
+      </Switch>
+    </div>
   )
 }
 
--- client/src/contexts/I18nProvider.tsx
+++ client/src/contexts/I18nProvider.tsx
@@ -1,8 +1,12 @@
-import React, { createContext, useContext } from 'react'
-import en from '../locales/en.json'
+import React, { createContext, useContext, useState, useMemo } from 'react'
+import enTranslation from '../locales/en.json'
+import esTranslation from '../locales/es.json'
 import AboutEN from '../components/internationalized/AboutEN'
+//import { useParams } from 'react-router-dom'
 
-type Translation = typeof en
+type Translation = typeof enTranslation
+
+
 
 class Locale {
     translation: Translation
@@ -13,21 +17,41 @@ class Locale {
     }
 }
 
-const defaultState : Locale = new Locale(en, { about: <AboutEN /> })
 
-const I18nContext = createContext<Locale>(defaultState)
+const en : Locale = new Locale(enTranslation, { about: <AboutEN /> })
+const es : Locale = new Locale(esTranslation, { about: <AboutEN /> })
+
+
+
+const locales : { [index: string] : Locale } = {
+    "en": en,
+    "es": es
+}
+
+
+const I18nContext = createContext<Locale>(en)
+
+const I18nMethodsContext = createContext<(x : string) => void>((_x) => null)
 
 const I18nProvider = ({ children }: { children: React.ReactNode }) => {
+  const [localeString, setLocale] = useState<string>("en")
+  const locale = locales[localeString] || en
+  const localeCallback = (x : string) => setLocale(x)
   return (
-    <I18nContext.Provider value={defaultState}>
+  <I18nMethodsContext.Provider value={localeCallback}>
+    <I18nContext.Provider value={locale}>
     {children}
     </I18nContext.Provider>
+  </I18nMethodsContext.Provider>
   )
 }
 
 type LocaleTransformer<T> = (v : Locale) => T
+
 export function useI18n<T>(f : LocaleTransformer<T>) : T {
     return f(useContext(I18nContext))
 }
+
+export const useI18nSetter = () => useContext(I18nMethodsContext)
 
 export default I18nProvider
