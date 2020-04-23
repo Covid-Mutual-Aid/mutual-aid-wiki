2f68a0cdee5e9d103afaa03c35bfb45dd6a4589e
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2020-04-23 14:47:19 +0200

    temporarily disable language switcher

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,17 +14,14 @@ import Report from './pages/Report'
 
 function App() {
   return (
-    <div>
-      <LocaleSwitcher />
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/add-group" component={CreateGroup} />
-        <Route path="/edit/:id/:token" component={EditGroup} />
-        <Route path="/edit/:id" component={EmailAuth} />
-        <Route path="/report/:id" component={Report} />
-        <Route path="/" component={Groups} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/about" component={About} />
+      <Route path="/add-group" component={CreateGroup} />
+      <Route path="/edit/:id/:token" component={EditGroup} />
+      <Route path="/edit/:id" component={EmailAuth} />
+      <Route path="/report/:id" component={Report} />
+      <Route path="/" component={Groups} />
+    </Switch>
   )
 }
 
