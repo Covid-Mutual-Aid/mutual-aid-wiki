99c9278e9b256c0ba1e6a0cd785089dbd31ad61e
Dan Beaven <dm.beaven@gmail.com>
2020-04-27 22:28:46 +0100

    small

--- client/src/components/SidePannel.tsx
+++ client/src/components/SidePannel.tsx
@@ -59,7 +59,7 @@ const MapIcon = styled.div`
   transition: box-shadow 0.3s;
 
   & > div {
-    transition: box-shadow 0.2s;
+    transition: box-shadow 0.3s;
     opacity: 0;
     margin-right: -3rem;
     transition: all 0.2s;
