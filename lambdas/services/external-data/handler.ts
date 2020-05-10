5244bc7a97f1ff25621bbb88fc4b8af54610c26c
Julian <julian.tapales@gmail.com>
2020-05-10 14:32:05 +0100

    Create covid19MutualAidGroupsUk source

--- lambdas/services/external-data/handler.ts
+++ lambdas/services/external-data/handler.ts
@@ -1,26 +1,29 @@
 import 'source-map-support/register'
 import { switchMap } from 'rxjs/operators'
+import { throwError } from 'rxjs'
 import P from 'ts-prove'
 
 import lambda, { params, responseJson$ } from '../_utility_/lib/lambdaRx'
-import db from '../_utility_/database'
 import { getData } from './sources/usa-localised-resources'
 import { testSource } from './sources/test-sheet'
+import { covid19MutualAidGroupsUk } from './sources/covid19MutualAidGroupsUk'
 
 export const getExternalData = lambda((req$) => req$.pipe(switchMap(getData), responseJson$))
 
 export const getTestData = lambda((req$) => req$.pipe(switchMap(testSource.handler), responseJson$))
+export const covid19MutualAidGroupsUkHandler = lambda((req$) =>
+  req$.pipe(switchMap(covid19MutualAidGroupsUk.handler), responseJson$)
+)
 
-const sources = [testSource]
+const sources = [testSource, covid19MutualAidGroupsUk]
 
 export const triggerSource = lambda((req$) =>
   req$.pipe(
     params(P.shape({ id: P.string })),
-    switchMap(async ({ id }) => {
+    switchMap(({ id }) => {
       const source = sources.find((s) => s.external_id === id)
-      if (typeof source === 'undefined') return { message: 'Invalid source' }
-      const snapShot = await source.handler()
-      return snapShot
+      if (typeof source === 'undefined') return throwError('Not valid id')
+      return source.handler()
     }),
     responseJson$
   )
