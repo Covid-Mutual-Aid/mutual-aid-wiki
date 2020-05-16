cf6333dfd98fc5ac82943997eb4006e9e14be2d3
Julian <julian.tapales@gmail.com>
2020-05-16 22:54:33 +0100

    Add migration script

--- scripts/migration-16-05-2020.js
+++ scripts/migration-16-05-2020.js
@@ -1,3 +1,15 @@
+/**
+ * This migration will:
+ * - Rename the country to location_country
+ * - Set location_country: country code
+ * - Add a source: mutualaidwiki
+ * - Set external: false
+ *
+ * Maybe we should:
+ * - rename link_facebook to link
+ * - Accept multiple kinds of links like in the email input (can be done in the future, but we should then turn links into an array of links)
+ */
+
 const child = require('child_process')
 const util = require('util')
 const path = require('path')
@@ -20,6 +32,9 @@ const writeBatch = (batch, i) => {
     .catch((err) => console.log(err))
     .then(() => util.promisify(fs.unlink)(fileP))
 }
+
+const omit = (keys, x) =>
+  Object.keys(x).reduce((all, key) => (keys.includes(key) ? all : { ...all, [key]: x[key] }), {})
 
 const geolocateCountry = (coord) =>
   axios
@@ -62,29 +77,25 @@ const geolocateCountries = (groups) => {
   return recurse(groups, Promise.resolve([]))
 }
 
-const withSource = () =>
-  util
-    .promisify(child.exec)(`${awsDynamodb} scan --table-name dev-groups10 > ${groupsFile}`)
-    .then(() => require(groupsFile))
-    .then((groups) =>
-      groups.Items.slice(0, 10).map((g) => ({
-        ...g,
-        source: {
-          S: 'mutual-aid-wiki',
-        },
-      }))
-    )
-    .then((g) => util.promisify(fs.writeFile)('with-source.json', JSON.stringify(g)))
+//also delete country field, because we are now calling it location_country
 
-const withCountries = () =>
+const withCountriesAndSource = () =>
   util
     .promisify(child.exec)(`${awsDynamodb} scan --table-name dev-groups10 > ${groupsFile}`)
     .then(() => require(groupsFile))
     .then((groups) => groups.Items.slice(0, 3))
+    .then((groups) =>
+      groups.map((g) => ({
+        ...omit(['country'], g),
+        source: {
+          S: 'mutualaidwiki',
+        },
+      }))
+    )
     .then(geolocateCountries)
-    .then((g) => util.promisify(fs.writeFile)('with-countries.json', JSON.stringify(g)))
+    .then((g) => util.promisify(fs.writeFile)('with-countries-and-source.json', JSON.stringify(g)))
 
-withCountries()
+withCountriesAndSource()
 
 // .then((g) => (g.country ? Promise.resolve(g) : { ...g, country: { S: '' } }))
 // .then((groups) =>
